2f91b774c731c058a122806157d8c09bd472ec8c
Cory <maurbeus@yahoo.com>
2021-10-18 02:51:26 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const softness = document.querySelector('.softness')
+const fosterButton = document.querySelector('.fosterButton');
+
+softness.addEventListener("click", e => {
+alert("Tian isn't afraid of anything. These statements are reckless and highly defamatory. Tian demands that you cease and desist from making such statements. If you do not, appropriate legal action will be taken. Govern yourself accordingly.")
+});
+
+fosterButton.addEventListener("click", e => {
+alert("This feature currently unavailable. Reason: Tian currently has a foster parent.")
+});
